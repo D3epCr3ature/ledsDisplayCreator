eaabe7123768d5c940df142a5d61a4f0fe0e9962
D3epCr3ature <kcs.bp@ikmail.com>
2025-05-14 00:51:37 +0200

    GUI: Add X-Ray toggle check box

--- 03b-Software/gui/mainwindow.h
+++ 03b-Software/gui/mainwindow.h
@@ -83,6 +83,7 @@ private:
 
     /* Interactives */
     QCheckBox   *logsCheckBox = nullptr;
+    QCheckBox   *xRayCheckBox = nullptr;
     QPushButton *logsClearBtn = nullptr;
 
     QComboBox *ledTypeDrpDn      = nullptr;
